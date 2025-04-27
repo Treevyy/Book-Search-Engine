6b37953907ca4b54b845cb9ad42654faa79d96af
Treevyy <aarontrevormoore@gmail.com>
2025-04-27 13:03:46 -0500

    fixed error

--- server/src/schema/resolvers.ts
+++ server/src/schema/resolvers.ts
@@ -1,6 +1,6 @@
 import type IUserContext from '../interfaces/UserContext.js';
 import type IUserDocument from '../interfaces/UserDocument.js';
-import type IBookInput from '../interfaces/BookInput.js';
+// import type IBookInput from '../interfaces/BookInput.js';
 import { User } from '../models/index.js';
 import { signToken, AuthenticationError } from '../services/auth.js';
 
