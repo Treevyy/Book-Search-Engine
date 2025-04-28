c15babc2d62e5846498daf4719add7fc4da57c71
Treevyy <aarontrevormoore@gmail.com>
2025-04-28 10:20:05 -0500

    reconfigured routes for build commands

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,12 +6,15 @@ import {ApolloServer} from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schema/index.js';
 import { authenticateToken } from './services/auth.js';
+import { fileURLToPath } from 'node:url';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -30,10 +33,10 @@ app.use('/graphql', expressMiddleware(server as any,
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
 
   app.get('*', (_req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 }
 
